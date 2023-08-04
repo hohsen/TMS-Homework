405953dee6e103135e347e9d5f3c15a76f123b1c
Vladislav <hohsens@gmail.com>
2023-08-04 11:22:59 +0300

    renamed directories for DZ_38 and DZ_39

---
+++ DZ_38_Laravel/database/seeders/StudentSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Student;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class StudentSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        Student::create([
+            'name' => 'Vlad',
+            'age' => 26,
+            'email' => 'vlad@tms.com',
+        ]);
+    }
+}
